4438cbbb1618913784ca2b25d30c37df3cb31735
KatsuShooter <daniil.kikimov123@mail.ru>
2020-09-21 22:51:32 +0600

    Initial Commit

---
+++ DentistCalendar/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  DentistCalendar
+//
+//  Created by Даник 💪 on 9/21/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ DentistCalendar/DentistCalendarApp.swift
@@ -0,0 +1,17 @@
+//
+//  DentistCalendarApp.swift
+//  DentistCalendar
+//
+//  Created by Даник 💪 on 9/21/20.
+//
+
+import SwiftUI
+
+@main
+struct DentistCalendarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
